904817b425f13ecffaac0f9c80fcacb53811f686
Samuel Ragusa <me@samragusa.com>
2017-07-19 20:06:01 -0400

    Added Tools for CNN Model Creation
    -Added a function to build an inception module based on a given shape
    -Added method to build fully connected layers given their shapes

--- win_loss_ann.py
+++ win_loss_ann.py
@@ -61,6 +61,83 @@ def cnn_model_fn(features, labels, mode):
     Model function for the CNN.
     """
     
+    def build_inception_module(the_input, module):
+        """
+        Module of the format:
+        [[[filters1_1,kernal_size1_1],... , [filters1_M,kernal_size1_M]],... ,
+            [filtersN_1,kernal_sizeN_1],... , [filtersN_P,kernal_sizeN_P]]
+            
+        (MAYBE IMPLEMENT) A set of [filtersJ_I, kernal_sizeJ_I] can be replaced by None for passing of input to concatination
+        
+        NOTES:
+        1) Add naming for use with TensorBoard
+        2)test to see if this works
+        """
+        
+        path_outputs = [None for _ in range(len(module))]  ##See if I can do [None]*len(module) instead
+        
+        cur_input = None
+        for j, path in enumerate(module):
+            for i, section in enumerate(path):
+                if i==0:
+                    if j != 0:
+                        path_outputs[j-1] = cur_input
+                        
+                    cur_input = the_input
+                
+                cur_input = tf.layers.conv2d(
+                    inputs=cur_input,
+                    filters=section[0],
+                    kernel_size=[section[1], section[1]],
+                    padding="same",
+                    activation=tf.nn.relu)
+        
+        path_outputs[-1] = cur_input
+        return tf.nn.relu(tf.concat([temp_input for temp_input in path_outputs],3))
+    
+    
+    def build_fully_connected_layers(the_input, shape, dropout_rates=None):
+        """
+        a function to build the fully connected layers onto the computational graph from
+        given specifications.
+        
+        Dropout_rates if kept as the default None will be 0 for every layer, if set to a scaler
+        between 0 (inclusive) and 1 (exclusive) will apply the given dropout rate to every layer 
+        being built, lastly dropout_rates can be an array the same size as the shape parameter,
+        where the dropout rate at index j will be applied to the layer with shape given at index
+        j of the shape parameter.  
+        
+        shape of the format:
+        [num_neurons_layer_1,num_neurons_layer_2,...,num_neurons_layer_n]
+        
+        NOTES:
+        1) Make sure altering the_input doesn't have a pointer related problem
+        2) Add in the error messages where written in comment
+        """
+        
+        if dropout_rates == None:
+            dropout_rates = [0]*len(shape)
+        elif not isinstance(dropout_rates, list):
+            if dropout_rates >= 0 and dropout_rates < 1:
+                dropout_rates = [dropout_rates]*len(shape)
+            else:
+                #PRINT THE ERROR
+                pass
+        else:
+            if len(dropout_rates) != len(shape):
+                #PRINT THE ERROR
+                pass       
+        
+        for size, dropout in zip(shape, dropout_rates):
+            temp_layer_dense = tf.layers.dense(inputs=the_input, units=size, activation=tf.nn.relu)
+            if dropout != 0:
+                the_input =  tf.layers.dropout(inputs=temp_layer_dense, rate=dropout, training=mode == learn.ModeKeys.TRAIN)
+            else:
+                the_input = temp_layer_dense
+        
+        return the_input
+    
+    
     input_layer = tf.reshape(features, [-1,8,8,1])
 
     conv1 = tf.layers.conv2d(
@@ -112,5 +189,4 @@ def cnn_model_fn(features, labels, mode):
     # Return a ModelFnOps object
     return model_fn_lib.ModelFnOps(
         mode=mode, predictions=predictions, loss=loss, train_op=train_op)
-
     
