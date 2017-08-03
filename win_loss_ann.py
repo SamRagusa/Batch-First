a23e2f4998f78b1c6135e0e3180803d65aa26c8d
Samuel Ragusa <me@samragusa.com>
2017-08-03 13:12:10 -0400

    Added numerous summaries for TensorBoard
    -Added proper name scoping and variable naming to inception module
    building function and dense layer building function
    -Added activation summaries to the layers built in the inception module
    building function
    -Added other minor TensorBoard integrations

--- win_loss_ann.py
+++ win_loss_ann.py
@@ -5,6 +5,7 @@ Created on Jul 2, 2017
 '''
 import tensorflow as tf
 from tensorflow.contrib import learn
+from tensorflow.contrib import layers   #different than the tf.layers methods called elsewhere
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 
@@ -55,38 +56,49 @@ def cnn_model_fn(features, labels, mode):
     Model function for the CNN.
     """
     
-    def build_inception_module(the_input, module):
+    def build_inception_module(the_input, module, activation_summaries=[], num_previously_built_inception_modules=0):
         """
+        Builds an inception module based on the design given to the function.  It returns the final layer in the module,
+        and the activation summaries generated for the layers within the inception module.
+        
+        The layers will be named "module_N_path_M/layer_P", where N is the inception module number, M is what path number it is on,
+        and P is what number layer it is in that path.      
+        
         Module of the format:
         [[[filters1_1,kernal_size1_1],... , [filters1_M,kernal_size1_M]],... ,
             [filtersN_1,kernal_sizeN_1],... , [filtersN_P,kernal_sizeN_P]]
             
         (MAYBE IMPLEMENT) A set of [filtersJ_I, kernal_sizeJ_I] can be replaced by None for passing of input to concatination
-        
-        NOTES:
-        1) Add naming for use with TensorBoard
         """
-        
-        path_outputs = [None for _ in range(len(module))]  ##See if I can do [None]*len(module) instead
+        path_outputs = [None for _ in range(len(module))]  ##SEE if I can do [None]*len(module) instead
         
         cur_input = None
         for j, path in enumerate(module):
-            for i, section in enumerate(path):
-                if i==0:
-                    if j != 0:
-                        path_outputs[j-1] = cur_input
-                        
-                    cur_input = the_input
-                
-                cur_input = tf.layers.conv2d(
-                    inputs=cur_input,
-                    filters=section[0],
-                    kernel_size=[section[1], section[1]],
-                    padding="same",
-                    activation=tf.nn.relu)
-        
+            with tf.name_scope("inception_module_" + str(num_previously_built_inception_modules + 1) + "_path_" + str(j+1)):
+                for i, section in enumerate(path):
+                    if i==0:
+                        if j != 0:
+                            path_outputs[j-1] = cur_input
+                            
+                        cur_input = the_input
+                    
+                    cur_input = tf.layers.conv2d(
+                        inputs=cur_input,
+                        filters=section[0],
+                        kernel_size=[section[1], section[1]],
+                        padding="same",
+                        activation=tf.nn.relu,
+                        name="inception_module_" + str(num_previously_built_inception_modules+1) + "_path_" + str(j+1) + "/layer_" + str(i+1))
+                    
+                    activation_summaries.append(layers.summarize_activation(cur_input))
+                    
         path_outputs[-1] = cur_input
-        return tf.nn.relu(tf.concat([temp_input for temp_input in path_outputs],3))
+        
+        with tf.name_scope("inception_module_" + str(num_previously_built_inception_modules + 1) + "_concat"):
+            final_layer = tf.nn.relu(tf.concat([temp_input for temp_input in path_outputs],3), name="inception_module_" + str(num_previously_built_inception_modules + 1) + "_concat")
+            activation_summaries.append(layers.summarize_activation(final_layer))
+            
+        return final_layer, activation_summaries
     
     def build_fully_connected_layers(the_input, shape, dropout_rates=None, num_previous_fully_connected_layers=0):
         """
@@ -107,43 +119,53 @@ def cnn_model_fn(features, labels, mode):
         2) Add in the error messages where written in comment
         """
         
-        if dropout_rates == None:
-            dropout_rates = [0]*len(shape)
-        elif not isinstance(dropout_rates, list):
-            if dropout_rates >= 0 and dropout_rates < 1:
-                dropout_rates = [dropout_rates]*len(shape)
+        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$CONSIDER PUTTING THIS WHOLE SECTION UNDER ONE name_scope$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+        with tf.name_scope("FC_" + str(num_previous_fully_connected_layers + 1)):
+            if dropout_rates == None:
+                dropout_rates = [0]*len(shape)
+            elif not isinstance(dropout_rates, list):
+                if dropout_rates >= 0 and dropout_rates < 1:
+                    dropout_rates = [dropout_rates]*len(shape)
+                else:
+                    #PRINT THE ERROR BETTER
+                    print("THIS ERROR NEEDS TO BE HANDLED BETTER!   1")
             else:
-                #PRINT THE ERROR
-                pass
-        else:
-            if len(dropout_rates) != len(shape):
-                #PRINT THE ERROR
-                pass
-                
-        
-        for index, size, dropout in zip(range(len(shape)),shape, dropout_rates):
-            #Figure out if instead I should use tf.layers.fully_connected instead of tf.layers.dense
-            temp_layer_dense = tf.layers.dense(inputs=the_input, units=size, activation=tf.nn.relu, name="FC_" + str(index + num_previous_fully_connected_layers))
-            if dropout != 0:
-                the_input =  tf.layers.dropout(inputs=temp_layer_dense, rate=dropout, training=mode == learn.ModeKeys.TRAIN)
-            else:
-                the_input = temp_layer_dense
-        
-        return the_input
+                if len(dropout_rates) != len(shape):
+                    #PRINT THE ERROR BETTER
+                    print("THIS ERROR NEEDS TO BE HANDLED BETTER!   2")
+                    
+            
+            for index, size, dropout in zip(range(len(shape)),shape, dropout_rates):
+                #Figure out if instead I should use tf.layers.fully_connected
+                temp_layer_dense = tf.layers.dense(inputs=the_input, units=size, activation=tf.nn.relu, name="FC_" + str(index + num_previous_fully_connected_layers + 1))
+                if dropout != 0:
+                    the_input =  tf.layers.dropout(inputs=temp_layer_dense, rate=dropout, training=mode == learn.ModeKeys.TRAIN)
+                else:
+                    the_input = temp_layer_dense
+            
+            return the_input
         
     
+    #set these constants up with something like flags to better be able
+    #to run modified versions of the model in the future (when tuning the model)
+    #
+    #for TRAIN_OP_SUMMARIES loss is an option but as of now it is being
+    #logged independently so that it can be easily added to another hook.
+    TRAIN_OP_SUMMARIES = ["learning_rate", "gradients", "gradient_norm"]
+    THE_OPTIMIZER = "SGD"
     NUM_OUTPUT_NEURONS = 2
     INCEPTION_1_SHAPE = [[[100,2]],[[150,3]],[[250,5]]]
     INCEPTION_2_SHAPE = [[[250,1]],[[250,1],[150,3]],[[250,1],[250,5]]]
-    DENSE_LAYERS_SHAPE = [500]#[2048,4096,512]
+    DENSE_LAYERS_SHAPE = [1024]#[2048,4096,512]
     DENSE_LAYERS_DROPOUT_RATES = .4
     LEARNING_RATE = .001
+    SAVE_SUMMARY_INTERVAL = 5
     
     input_layer = tf.reshape(features, [-1,8,8,1])
-
-    inception_module1 = build_inception_module(input_layer, INCEPTION_1_SHAPE)
     
-    inception_module2 = build_inception_module(inception_module1, INCEPTION_2_SHAPE)
+    inception_module1, activation_summaries = build_inception_module(input_layer, INCEPTION_1_SHAPE)
+    
+    inception_module2, activation_summaries = build_inception_module(inception_module1, INCEPTION_2_SHAPE, activation_summaries,1)
 
     #650 is the sum of the number of features in the last
     #section of each "path" in the inception layer.
@@ -157,7 +179,7 @@ def cnn_model_fn(features, labels, mode):
         DENSE_LAYERS_DROPOUT_RATES)
     
     
-    logits = tf.layers.dense(inputs=dense_layers_outputs, units=NUM_OUTPUT_NEURONS)
+    logits = tf.layers.dense(inputs=dense_layers_outputs, units=NUM_OUTPUT_NEURONS, name="FC_" + str(len(DENSE_LAYERS_SHAPE)+1))
     
     loss = None
     train_op = None
@@ -166,14 +188,16 @@ def cnn_model_fn(features, labels, mode):
     if mode != learn.ModeKeys.INFER:
         loss = tf.losses.softmax_cross_entropy(
             onehot_labels=labels, logits=logits)
+        tf.summary.scalar('loss_summary', loss)
         
     # Configure the Training Op (for TRAIN mode)
     if mode == learn.ModeKeys.TRAIN:
-        train_op = tf.contrib.layers.optimize_loss(
+        train_op = layers.optimize_loss(
             loss=loss,
             global_step=tf.contrib.framework.get_global_step(),
             learning_rate=LEARNING_RATE,
-            optimizer="SGD")
+            optimizer=THE_OPTIMIZER,
+            summaries = TRAIN_OP_SUMMARIES)
         
     # Generate Predictions
     predictions = {
@@ -182,8 +206,10 @@ def cnn_model_fn(features, labels, mode):
         logits, name="softmax_tensor")
     }
         
-    summaries = tf.contrib.layers.summarize_tensors(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))
-    summary_hook = tf.train.SummarySaverHook(save_steps = 2, output_dir ="/tmp/win_loss_ann", summary_op=summaries)
+    #Create the trainable variable summaries and merge them together to give to a hook
+    trainable_var_summaries = layers.summarize_tensors(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))  #Not sure if needs to be set as a variable, should check
+    merged_summaries = tf.summary.merge_all()
+    summary_hook = tf.train.SummarySaverHook(save_steps = SAVE_SUMMARY_INTERVAL, output_dir ="/tmp/win_loss_ann", summary_op=merged_summaries)
 
     # Return a ModelFnOps object
     return model_fn_lib.ModelFnOps(
@@ -194,13 +220,17 @@ def main(unused_param):
     """
     Sets up the data pipeline, creates the computational graph, and trains the model.
     """
+    
+    #set these constants up with something like flags to better be able
+    #to run modified versions of the model in the future (when tuning the model)
     TRAINING_DATA_FILENAME = "small_dataset.csv"
     SAVE_MODEL_DIR = "/tmp/win_loss_ann1"
-    BATCH_SIZE = 200
+    BATCH_SIZE = 100  #NEED TO SET THIS
     MIN_AFTER_DEQUEUE = 1000
-    NUM_EPOCHS = 10
+    NUM_EPOCHS = 200
     PRINT_ITERATION_INTERVAL = 2
-    LOG_ITERATION_INTERVAL = 2
+    SNAPSHOT_INTERVAL = 50
+    #LOG_ITERATION_INTERVAL = 2
    
     
     # Set up the training data pipeline
@@ -237,20 +267,17 @@ def main(unused_param):
         model_dir=SAVE_MODEL_DIR)
     
     # Set up logging for predictions
-    tensors_to_print = {"probabilities": "softmax_tensor"} #Picked randomly for learning purposes
+    tensors_to_print = {"loss": "softmax_cross_entropy_loss/value:0"}
     print_logging_hook = tf.train.LoggingTensorHook(
         tensors=tensors_to_print,
         every_n_iter=PRINT_ITERATION_INTERVAL)
-    
-#     save_logging_hook = tf.train.SummarySaverHook(
-#         save_steps = LOG_ITERATION_INTERVAL,
-#         output_dir=SAVE_MODEL_DIR,
-#         summary_op = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="temp"))
 
     # Train the model
     classifier.fit(
         input_fn=lambda: input_data_fn(training_data_pipe_ops),
-        steps=NUM_EPOCHS)
+        steps=NUM_EPOCHS,
+        monitors=[print_logging_hook],
+        snapshot_step=SNAPSHOT_INTERVAL)
     
 
 if __name__ == "__main__":
