9247410400c655fe823d455ec7e04eaf5ff27143
Sam Ragusa <me@SamRagusa.com>
2017-08-09 14:40:22 -0400

    Removed Depreciated Code and Implemented Fixes
    -Created accuracy metric (for MetricSpec) because TensorFlow metrics haven't been working right
    -Updated the test data evaluation to no longer depend on depreciated parameters
    -Added ability to add a testing data pipeline (and implemented it)
    -Added params to cnn_model_fn so all hyper-parameters are set within the "main" function
    -Removed many depreciated hooks
    -Implemented hooks to replace some of the depreciated hooks previously used
    -Added some better commenting

--- win_loss_ann.py
+++ win_loss_ann.py
@@ -5,7 +5,7 @@ Created on Jul 2, 2017
 '''
 import tensorflow as tf
 from tensorflow.contrib import learn
-from tensorflow.contrib import layers   #different than the tf.layers methods called elsewhere
+from tensorflow.contrib import layers
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 
@@ -52,7 +52,7 @@ def data_pipeline(filenames, batch_size, num_epochs=None, min_after_dequeue=1000
         return example_batch, label_batch
 
 
-def cnn_model_fn(features, labels, mode):
+def cnn_model_fn(features, labels, mode, params):
     """
     Model function for the CNN.
     """
@@ -71,7 +71,7 @@ def cnn_model_fn(features, labels, mode):
             
         (MAYBE IMPLEMENT) A set of [filtersJ_I, kernal_sizeJ_I] can be replaced by None for passing of input to concatination
         """
-        path_outputs = [None for _ in range(len(module))]  ##SEE if I can do [None]*len(module) instead
+        path_outputs = [None for _ in range(len(module))]  #See if I can do [None]*len(module) instead
         
         cur_input = None
         for j, path in enumerate(module):
@@ -97,7 +97,8 @@ def cnn_model_fn(features, labels, mode):
         
         with tf.name_scope("inception_module_" + str(num_previously_built_inception_modules + 1) + "_concat"):
             final_layer = tf.nn.relu(tf.concat([temp_input for temp_input in path_outputs],3), name="inception_module_" + str(num_previously_built_inception_modules + 1) + "_concat")
-            activation_summaries.append(layers.summarize_activation(final_layer))
+            #Currently not doing this activaton summary because as of now it is not very helpful.
+            #activation_summaries.append(layers.summarize_activation(final_layer))
             
         return final_layer, activation_summaries
     
@@ -117,11 +118,8 @@ def cnn_model_fn(features, labels, mode):
         [num_neurons_layer_1,num_neurons_layer_2,...,num_neurons_layer_n]
         
         NOTES:
-        1) Make sure altering the_input doesn't have a pointer related problem
-        2) Add in the error messages where written in comment
+        1) Add in the error messages where written in comment
         """
-        
-        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$CONSIDER PUTTING THIS WHOLE SECTION UNDER ONE name_scope$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
         with tf.name_scope("FC_" + str(num_previous_fully_connected_layers + 1)):
             if dropout_rates == None:
                 dropout_rates = [0]*len(shape)
@@ -148,28 +146,26 @@ def cnn_model_fn(features, labels, mode):
             return the_input
         
     
-    #set these constants up with something like flags to better be able
-    #to run modified versions of the model in the future (when tuning the model)
-    #
     #for TRAIN_OP_SUMMARIES loss is an option but as of now it is being
     #logged independently so that it can be easily added to another hook.
     TRAIN_OP_SUMMARIES = ["learning_rate", "gradients", "gradient_norm"]
-    THE_OPTIMIZER = "SGD"
-    NUM_OUTPUT_NEURONS = 2
-    INCEPTION_1_SHAPE = [[[200,2]],[[300,3]],[[500,5]]]
-    INCEPTION_2_SHAPE = [[[500,1]],[[500,1],[300,3]],[[500,1],[500,5]]]
-    DENSE_LAYERS_SHAPE = [2048,4096,512]
-    DENSE_LAYERS_DROPOUT_RATES = .4
-    LEARNING_RATE = .001
-    SAVE_SUMMARY_INTERVAL = 5
+    THE_OPTIMIZER = params['optimizer']
+    NUM_OUTPUT_NEURONS = params['num_outputs']
+    INCEPTION_1_SHAPE = params['inception_module1']
+    INCEPTION_2_SHAPE = params['inception_module2']
+    DENSE_LAYERS_SHAPE = params['dense_shape']
+    DENSE_LAYERS_DROPOUT_RATES = params['dense_dropout']
+    LEARNING_RATE = params['learning_rate']
+    SAVE_SUMMARY_INTERVAL = params['log_interval']
+    SAVE_MODEL_DIR = params['model_dir']
 
-    
-    #activation_summaries = []
-    
+    #Reshaped the input data to be like a chess board (8x8) 
     input_layer = tf.reshape(features, [-1,8,8,1])
 
+    #Build the first inception module
     inception_module1, activation_summaries = build_inception_module(input_layer, INCEPTION_1_SHAPE)
     
+    #Build the second inception module
     inception_module2, activation_summaries = build_inception_module(inception_module1, INCEPTION_2_SHAPE, activation_summaries,1)
 
     #1300 is the sum of the number of features in the last
@@ -177,15 +173,21 @@ def cnn_model_fn(features, labels, mode):
     #The 8s are the dimensions of each feature map
     FIGURE_THIS_OUT_AS_FUNCTION_OF_CONSTANTS = 1300*8*8  
     
+    #Reshape the output from the convolutional layers for the densely connected layers
     flat_conv_output = tf.reshape(inception_module2, [-1, FIGURE_THIS_OUT_AS_FUNCTION_OF_CONSTANTS])
+    
+    #Build the fully connected layers 
     dense_layers_outputs = build_fully_connected_layers(
         flat_conv_output,
         DENSE_LAYERS_SHAPE,
         DENSE_LAYERS_DROPOUT_RATES)
     
     
+    #Create the final layer of the ANN
     logits = tf.layers.dense(inputs=dense_layers_outputs, units=NUM_OUTPUT_NEURONS, name="FC_" + str(len(DENSE_LAYERS_SHAPE)+1))
     
+    
+    #Figure out if these are needed.  Don't think so, but don't have time to think about it right now
     loss = None
     train_op = None
     
@@ -214,7 +216,7 @@ def cnn_model_fn(features, labels, mode):
     #Create the trainable variable summaries and merge them together to give to a hook
     trainable_var_summaries = layers.summarize_tensors(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES))  #Not sure if needs to be set as a variable, should check
     merged_summaries = tf.summary.merge_all()
-    summary_hook = tf.train.SummarySaverHook(save_steps = SAVE_SUMMARY_INTERVAL, output_dir ="/tmp/win_loss_ann", summary_op=merged_summaries)
+    summary_hook = tf.train.SummarySaverHook(save_steps = SAVE_SUMMARY_INTERVAL, output_dir =SAVE_MODEL_DIR, summary_op=merged_summaries)
     
     # Return a ModelFnOps object
     return model_fn_lib.ModelFnOps(
@@ -225,94 +227,126 @@ def cnn_model_fn(features, labels, mode):
         training_hooks=[summary_hook])
     
 
+
 def main(unused_param):
     """
-    Sets up the data pipeline, creates the computational graph, and trains the model.
+    Set up the data pipelines, create the computational graph, train the model, and evaluate the results.
     """
     
-    #set these constants up with something like flags to better be able
-    #to run modified versions of the model in the future (when tuning the model)
-    TRAINING_DATA_FILENAME = "tensor_flow_input_database.csv"
-    SAVE_MODEL_DIR = "/tmp/win_loss_ann123"
-    VALIDATION_DATA = None              #Need to load these up, likely should keep them in RAM because it'd be quick
-    VALIDATION_LABELS = None            #Need to load these up, likely should keep them in RAM because it'd be quick
-    TEST_DATA = None                    #Need to load these up, likely should keep them in RAM because it'd be quick
-    TEST_LABELS = None                  #Need to load these up, likely should keep them in RAM because it'd be quick
-    BATCH_SIZE = 500  #NEED TO SET THIS
-    MIN_AFTER_DEQUEUE = 1000
-    NUM_EPOCHS = 200
-    PRINT_ITERATION_INTERVAL = 2
-    SNAPSHOT_INTERVAL = 25
-    VALIDATION_INTERVAL = 50
-    #LOG_ITERATION_INTERVAL = 2
-   
+    def accuracy_metric(predictions, labels,weights=None):
+        """
+        An accuracy metric built to be able to given to tf.contrib.learn.MetricSpec
+        as a metric.
+        
+        This is only used because the TensorFlow built in metrics haven't been working.
+        """
+        return tf.reduce_mean(tf.cast(tf.equal(predictions,tf.argmax(labels, 1)),tf.float32))
     
-    # Load training and eval data
-    training_data_pipe_ops = data_pipeline(    #MAKE SURE THAT THIS WILL WORK WHEN DONE ON MANY ITERATIONS
-        [TRAINING_DATA_FILENAME],
-        BATCH_SIZE, 
-        min_after_dequeue=MIN_AFTER_DEQUEUE)
     
     def input_data_fn(data_getter_ops):
         """
-        The function which is called to get data for the fit function.
+        The function which is called to get data for the fit functSion.
         
         NOTES:
-        1) Must make sure that this is not greatly slowing everything down
-        2) Should make this and all other data pipeline graph operations
-            run on CPU while the GPUs do the heavy math operations
-        3) Figure out if I can start the coordinator and the queue runners
-            and continue getting batches until the epoch is complete, then 
-            request_stop() and join the thread.  Need to reread the queue_runner
-            guides on TensorFlow's website.
+        1) Need to build a better method for keeping track of threads.  This is
+        very important for running over multiple epochs.
         """
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
+#         coord = tf.train.Coordinator()
+#         threads = tf.train.start_queue_runners(coord=coord)
         batch, labels = sess.run(data_getter_ops)
-        coord.request_stop()
-        coord.join(threads)
+#         coord.request_stop()
+#         coord.join(threads)
         
-        return tf.constant(batch, dtype=tf.float32), tf.constant(labels, tf.int32)
-        
+        #See if these tf.constant() calls are needed or if I should have them be computed elsewhere
+        return tf.constant(batch, dtype=tf.float32), tf.constant(labels, dtype=tf.float32)
     
-    # Create the Estimator
+    #Hopefully set these constants up with something like flags to better be able
+    #to run modified versions of the model in the future (when tuning the model)
+    SAVE_MODEL_DIR = "/tmp/win_loss_ann"
+    TRAINING_DATA_FILENAME = "train_data.csv"
+    VALIDATION_FILENAME = "validation_data.csv"
+    TESTING_FILENAME = "test_data.csv"
+    NUM_OUTPUTS = 2
+    DENSE_SHAPE = [500]#[2048,4096,512]
+    DENSE_DROPOUT = .4   #NEED TO PICK THIS PROPERLY
+    OPTIMIZER = "SGD"    #NEED TO PICK THIS PROPERLY
+    TRAINING_MIN_AFTER_DEQUEUE = 1500      
+    TESTING_MIN_AFTER_DEQUEUE = 2000
+    TRAINING_BATCH_SIZE = 500  #NEED TO PICK THIS PROPERLY
+    TESTING_BATCH_SIZE = 1000
+    NUM_TESTING_STEPS = 200       #Should be set to number_of_testing_examples//TESTING_BATCH_SIZE
+    NUM_EPOCHS =  2000     #I think where I'm passing this parameter to isn't actually epochs, it's batches
+    LOG_ITERATION_INTERVAL = 100
+    INCEPTION_MODULE_1_SHAPE = [[[200,2]],[[300,3]],[[500,5]]]
+    INCEPTION_MODULE_2_SHAPE = [[[500,1]],[[500,1],[300,3]],[[500,1],[500,5]]]
+    LEARNING_RATE = .001   #NEED TO PICK THIS PROPERLY
+    
+    #Create training data pipeline
+    training_data_pipe_ops = data_pipeline(    #MAKE SURE THAT THIS WILL WORK WHEN DONE ON MANY ITERATIONS (because I'm pretty sure it won't)
+        [TRAINING_DATA_FILENAME],
+        TRAINING_BATCH_SIZE, 
+        min_after_dequeue=TRAINING_MIN_AFTER_DEQUEUE)
+    
+    #Create testing data pipeline
+    testing_data_pipe_ops = data_pipeline(
+        [TESTING_FILENAME],
+        TESTING_BATCH_SIZE,
+        min_after_dequeue=TRAINING_MIN_AFTER_DEQUEUE)
+
+    
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    
+    
+
+    #Create a Config object such that during training the saves and logs will be done at the intervals I want
+    the_config = tf.estimator.RunConfig().replace(save_checkpoints_steps=LOG_ITERATION_INTERVAL, save_summary_steps=LOG_ITERATION_INTERVAL)
+    
+    
+    #Create the Estimator
     classifier = learn.Estimator(
         model_fn=cnn_model_fn,
-        model_dir=SAVE_MODEL_DIR)
+        model_dir=SAVE_MODEL_DIR,
+        config = the_config,
+        params = {
+            'dense_shape': DENSE_SHAPE,
+            'dense_dropout' : DENSE_DROPOUT,
+            'optimizer' : OPTIMIZER,
+            'num_outputs' : NUM_OUTPUTS,
+            'log_interval': LOG_ITERATION_INTERVAL,
+            'model_dir' : SAVE_MODEL_DIR,
+            'inception_module1' : INCEPTION_MODULE_1_SHAPE,
+            'inception_module2' : INCEPTION_MODULE_2_SHAPE,
+            "learning_rate": LEARNING_RATE})
     
-    checkpoint_saver = tf.train.CheckpointSaverHook(
-        SAVE_MODEL_DIR,
-        save_steps=SNAPSHOT_INTERVAL)
-    
-    # Set up logging for predictions
-    tensors_to_print = {"loss": "softmax_cross_entropy_loss/value:0"}
-    print_logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_print,
-        every_n_iter=PRINT_ITERATION_INTERVAL)
     
     #Set up testing of validation data at a given interval
-    validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
-        VALIDATION_DATA,
-        VALIDATION_LABELS,
-        every_n_steps=VALIDATION_INTERVAL)
+#     validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+#         VALIDATION_DATA,
+#         VALIDATION_LABELS,
+# #         metrics = validation_metrics,
+#         every_n_steps=LOG_ITERATION_INTERVAL)
     
+
+    #fit the model to the training data given 
     classifier.fit(
         input_fn=lambda: input_data_fn(training_data_pipe_ops),
-        steps=NUM_EPOCHS,
-        monitors=[validation_monitor, print_logging_hook, checkpoint_saver])
-    
-    
-    # Configure the accuracy metric for evaluation
-    metrics = {
-        "accuracy": learn.MetricSpec(
-            metric_fn=tf.metrics.accuracy, prediction_key="classes"),
-    }
-    
-    # Evaluate the model and print results
+        steps=NUM_EPOCHS)
+
+    #Configure the accuracy metric for evaluation 
+    testing_metrics = {
+        "prediction accuracy": learn.MetricSpec(
+            metric_fn= accuracy_metric,
+            prediction_key="classes")}
+
+    #Evaluate the model and print results
     eval_results = classifier.evaluate(
-        x=TEST_DATA,
-        y=TEST_LABELS,
-        metrics=metrics)
+        input_fn=lambda: input_data_fn(testing_data_pipe_ops),
+        metrics=testing_metrics,
+        steps=NUM_TESTING_STEPS)  #steps is here so that the data pipeline doesn't make it hang
+    
+    coord.request_stop()
+    coord.join(threads)
     
     print(eval_results)
 
