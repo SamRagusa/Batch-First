2327be267f28b8fd896a7eb67d85bc84dd853731
Samuel Ragusa <me@samragusa.com>
2017-09-08 18:40:05 -0400

    Implemented Transposition Tables
    -Implemented TranspositionTableEntry class
    -Added use of a Transposition Table during the negamax search
    -Optimized testing heuristic for faster computation
    -Moved the testing heuristic inside of the Negamax class for speed
    increase

--- negamax.py
+++ negamax.py
@@ -4,10 +4,12 @@ Created on Sep 1, 2017
 @author: SamRagusa
 '''
 
-import chess    
+import chess
 # import Human_Player
 # from Random_AI import Random_AI
 import random
+import time
+from functools import reduce
 
 class Player:
     """
@@ -32,7 +34,24 @@ class Player:
         Makes a move on the Player object's chess.Board instance.
         """
         pass
+    
+    
+    
 
+class TranspositionTableEntry:
+    """
+    A class to store information about a node (board configuration) that has been 
+    previously computed.  It is used as the value in the transposition table.
+    """
+    def __init__(self, flag, value, depth):
+        """
+        INFO:
+        1) The flag value is None if containing a value, True if containing
+        an upperbound, and False if containing a lowerbound.
+        """
+        self.flag = flag
+        self.depth = depth
+        self.value = value
 
 
 
@@ -42,6 +61,8 @@ class Negamax(Player):
     A class representing a Chess playing AI implementing Negamax tree search with Alpha-Beta pruning.   
     """
     
+    
+    
     def __init__(self, is_white, the_depth, board_heuristic, the_board=None):
         """
         Initialize the instance variables to be stored by the AI. 
@@ -49,10 +70,18 @@ class Negamax(Player):
         self._board = the_board
         self._depth = the_depth
         self._is_white = is_white
-        self._heuristic = board_heuristic
+        if board_heuristic is None:
+            self._heuristic = self.heuristic_creator()
+        else:
+            self._heuristic = board_heuristic
+#         self._heuristic = board_heuristic
+        
+#         self.TEMP_TT_TABLE_USE_COUNTER =0
+        
+        self._tt = {}
     
     
-        #not inf and -inf to try and fix the problem involving try statement in make_move method
+        #not inf and -inf to try and fix the problem with try statement in make_move method
         WIN_VAL = 9999999
         LOSE_VAL = -9999999
         TIE_VAL = 0
@@ -62,20 +91,37 @@ class Negamax(Player):
         else:
             self._outcome_map = {"1-0" : LOSE_VAL, "0-1" : WIN_VAL, "1/2-1/2" : TIE_VAL}
 
-
     def negamax(self, depth, alpha, beta, color):
         """
-        A method implementing negamax with alpha-beta pruning to decide what move to make given 
-        the current board configuration. 
+        A method implementing negamax tree search with alpha-beta pruning and
+        transposition tables to decide what move to make given the current
+        board configuration. 
+        """   
         
-        NOTES:
-        1) Implement Transposition Tables
-        """
+        old_alpha = alpha
+        
+        cur_board_fen = self._board.board_fen()
+        
+        #Checks the transposition table for information on the current node
+        tt_entry = self._tt.get(cur_board_fen)
+        if not tt_entry is None and tt_entry.depth >= depth:
+#             self.TEMP_TT_TABLE_USE_COUNTER = self.TEMP_TT_TABLE_USE_COUNTER + 1
+            if tt_entry.flag is None:
+                return tt_entry.value
+            elif tt_entry.flag:
+                alpha = max(alpha, tt_entry.value)
+            else:
+                beta = min(beta, tt_entry.value)
+            
+            if alpha >= beta:
+                return tt_entry.value
+            
+        
         if self._board.is_game_over():
             return color * self._outcome_map[self._board.result()]
 
         if depth == 0:
-            return color * self._heuristic(self._board)
+            return color * self._heuristic()
         
         
         best_value = float('-inf')
@@ -85,22 +131,38 @@ class Negamax(Player):
             best_value = max(best_value, v)
             alpha = max(alpha, v)
             self._board.pop()
-            
             if alpha >= beta:
                 break
+            
+
+        #Update the transposition table
+        if best_value <= old_alpha:
+            self._tt[cur_board_fen] = TranspositionTableEntry(True, best_value, depth)
+        elif best_value >= beta:
+            self._tt[cur_board_fen] = TranspositionTableEntry(False, best_value, depth)
+        else:
+            self._tt[cur_board_fen] = TranspositionTableEntry(None, best_value, depth)
+        
+        
         return best_value
         
 
     def make_move(self):
         """
         TO-DO:
-        1) Figure out the cause of the try statement requirement and how to eliminate it
+        1) **IMPORTANT** Eliminate try statement 
+        2) Add current node to TT after computations
         """
         move_to_make = None
         alpha = float('-inf')
+#         beta = float('inf')
         v = float('-inf')
         
+#         moves = self._board.legal_moves
+#         num_moves_prev = len(self._board.legal_moves)
+#         move_counter = 0
         for move in self._board.legal_moves:
+#             move_counter = move_counter +1
             self._board.push(move)
             negamax_results = -self.negamax(self._depth-1, float('-inf'), -alpha, -1)
             if negamax_results > v:
@@ -109,36 +171,52 @@ class Negamax(Player):
             alpha = max(alpha, v)
             self._board.pop()
         try:
+#             if num_moves_prev != move_counter:
+#                 print("Error!  Move list is being altered!")
             self._board.push(move_to_make)
         except:
             temp_legal_move_list = self._board.legal_moves
+#             if num_moves_prev != move_counter:
+#                 print("Error!  Move list is being altered!")
 #             print(move_to_make)
 #             print(self._board)
 #             print(self._board.is_game_over())
-#             print(len(temp_legal_move_list))
+#             print("Number of moves:", len(self._board.legal_moves))
 #             print("is white:", self._is_white)
 
             #Makes a random move because this error shouldn't occur in the long term
             self._board.push(list(temp_legal_move_list)[random.randint(0,len(temp_legal_move_list)-1)])
+            
+    def heuristic_creator(self, piece_vals=[.1, .3,.45,.6,.9], random_decrease=.2):
+        """
+        Creates a basic heuristic if none was supplied.
+        """
+        def heuristic():
+            score = reduce(
+                lambda x,y: x+y,
+                map(
+                    lambda a,b,c: a*(b-c),
+                    piece_vals,
+                    map(
+                        lambda d: len(self._board.pieces(d, self._is_white)),
+                        [1,2,3,4,5]),
+                    map(
+                        lambda d: len(self._board.pieces(d, not self._is_white)),
+                        [1,2,3,4,5])))
+            
+            return score * (1 - random_decrease + random_decrease*random.random())
+        
+        return heuristic
         
 
 
-def test_heuristic_generator(is_white, piece_vals=[.1, .3,.45,.6,.9], random_decrease=.2):
-    """
-    Generates and returns a basic heuristic for board evaluation.
-    """
-    def test_heuristic(board):
-        score = 0
-        for index, val in enumerate(piece_vals):
-            score = score + val * (len(board.pieces(index + 1,is_white)) - len(board.pieces(index + 1,not is_white)))
-        return score * (1 - random_decrease + random_decrease*random.random())
-    return test_heuristic
-    
-    
+ 
+
+ 
 board =chess.Board()
  
-better_ai = Negamax(True, 3, test_heuristic_generator(True), board)
-worse_ai = Negamax(False, 2, test_heuristic_generator(False), board)
+better_ai = Negamax(True, 3, None, board)
+worse_ai = Negamax(False, 2, None, board)
 # rand = Random_AI()
 # human = Human_Player.Human_Player()
 # human.set_board(board)
@@ -146,17 +224,24 @@ worse_ai = Negamax(False, 2, test_heuristic_generator(False), board)
  
 better_ai_wins = 0
 worse_ai_wins = 0
-NUM_GAMES = 6
+NUM_GAMES = 200
 move_counter = 0
 PRINT_EVERY_MOVE = False
-STEP_PRINT_INCREMENT = 100
-GAME_PRINT_INCREMENT = 1
+PRINT_AT_START_OF_GAME = True
+PRINT_AT_MOVE_INCREMENT = False
+STEP_PRINT_INCREMENT = 500
+GAME_PRINT_INCREMENT = 10
+last_time = None
 for game_num in range(NUM_GAMES):
-    if game_num % GAME_PRINT_INCREMENT ==0:
+    if PRINT_AT_START_OF_GAME and game_num % GAME_PRINT_INCREMENT ==0:
         print("Starting game number", game_num)
     while not board.is_game_over():
-        if move_counter % STEP_PRINT_INCREMENT == 0:
-            print("Making move number", move_counter)
+        if PRINT_AT_MOVE_INCREMENT and move_counter % STEP_PRINT_INCREMENT == 0:
+            if move_counter == 0:
+                print("Making move number", move_counter)
+            else:
+                print("Making move number:", move_counter, ", average time per move: ", (time.time()-last_time)/STEP_PRINT_INCREMENT)
+            last_time = time.time()
         
         move_counter = move_counter + 1
         better_ai.make_move()
@@ -165,8 +250,9 @@ for game_num in range(NUM_GAMES):
             print(board, "\n")
      
         if not board.is_game_over():
-            if move_counter % STEP_PRINT_INCREMENT == 0:
-                print("Making move number", move_counter)
+            if PRINT_AT_MOVE_INCREMENT and move_counter % STEP_PRINT_INCREMENT == 0:
+                print("Making move number:", move_counter, ", average time per move: ", (time.time()-last_time)/STEP_PRINT_INCREMENT)
+                last_time = time.time()
             
             move_counter = move_counter + 1
             worse_ai.make_move()
@@ -180,6 +266,9 @@ for game_num in range(NUM_GAMES):
         worse_ai_wins = worse_ai_wins + 1
      
     board.reset()
+    
+
+
         
         
 print("Better player number of wins: ", better_ai_wins)
@@ -187,6 +276,7 @@ print("Worse player number of wins: ", worse_ai_wins)
 print("Number of tie games ", NUM_GAMES - better_ai_wins - worse_ai_wins)
 print()
 print("Average moves per game:", move_counter/NUM_GAMES)
-
+# print("Better AI used ", better_ai.TEMP_TT_TABLE_USE_COUNTER ,"TT lookups used")
+# print("Worse AI used ", worse_ai.TEMP_TT_TABLE_USE_COUNTER,"TT lookups used")
 
 
